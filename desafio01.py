46682d8ca69c7fb86c914b10629e467e2e3cbdd6
Janaína Rezende <janainarezende13@hotmail.com>
2021-12-09 13:32:50 -0300

    testando

---
+++ desafio01.py
@@ -0,0 +1,22 @@
+#desafio 1
+
+nome = input('Qual é o seu nome?')
+print('Olá,',nome,',Prazer em te conhecer!')
+
+
+
+#desafio 2
+
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print('Você nasceu no',dia,'de',mes,'de',ano,'. Correto?
+
+
+
+#desafio 3
+
+n1 = int(input('Informe o primeiro número:  '))
+n2 = int(input('Informe o segundo número:  '))
+print('A soma dos números é',n1+n2)
+      
